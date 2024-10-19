18e07d2ff023defff76b58314a8d1ceb18ac6053
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2024-10-19 16:25:16 +0330

    feat(databse, package.json, contanst): 1.data schema completed 2.watch command for server script 3.topics url added

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,13 @@
 export const BASE_URL = "http://localhost:3000";
 
-export const USER_URL = BASE_URL + '/users';
+export const USER_URL = BASE_URL + "/users";
 
-export const TESTS_URL = BASE_URL + '/tests'
+export const TESTS_URL = BASE_URL + "/tests";
 
-export const PROFILES_URL = BASE_URL + '/profiles'
+export const PROFILES_URL = BASE_URL + "/profiles";
 
-export const QUESTIONS_URL = BASE_URL + '/questions'
+export const QUESTIONS_URL = BASE_URL + "/questions";
+
+export const TOPICS_URL = BASE_URL + "/topics";
+
+//request form ?id_gte=7&id_lte=12&topic=react
