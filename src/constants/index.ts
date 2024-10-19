5ef28bc4b63f3811fdd1d685d8d5a8d153bbb977
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2024-10-19 15:48:00 +0330

    feat(database): completing the full schema for necessary access to stored data

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+export const BASE_URL = "http://localhost:3000";
+
+export const USER_URL = BASE_URL + '/users';
+
+export const TESTS_URL = BASE_URL + '/tests'
+
+export const PROFILES_URL = BASE_URL + '/profiles'
+
+export const QUESTIONS_URL = BASE_URL + '/questions'
